d073b98b849070d74c2c080d4b29dc7cbb503f29
eltanketeexplica-cmd <eltanketeexplica@gmail.com>
2026-05-15 09:27:39 -0300

    Add files via upload

---
+++ src/lib/stripe.ts
@@ -0,0 +1,7 @@
+import Stripe from 'stripe';
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2025-01-27' as any, // Use latest
+});
+
+export const PRO_PLAN_PRICE_ID = 'price_H5gg...' // Placeholder
